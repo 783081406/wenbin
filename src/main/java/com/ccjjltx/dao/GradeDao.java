6d94b593ffda808216ce4b93bcd9c70090f87cd5
783081406 <783081406ccj@gmail.com>
2018-02-18 17:51:37 +0800

    增加saveUpdate和deleteAid方法

--- src/main/java/com/ccjjltx/dao/GradeDao.java
+++ src/main/java/com/ccjjltx/dao/GradeDao.java
@@ -1,5 +1,8 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Activity;
+import com.ccjjltx.domain.Grade;
+import com.ccjjltx.domain.User;
 import com.ccjjltx.util.ToString;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -22,6 +25,8 @@ import java.util.List;
 public class GradeDao {
     @Resource(name = "sessionFactory")
     private SessionFactory factory;
+    @Resource(name = "userDao")
+    private UserDao userDao;
 
     /**
      * 得到所有参加人员的id
@@ -37,4 +42,30 @@ public class GradeDao {
         return ToString.parse(list);
     }
 
+    /**
+     * 删除aid的所有数据
+     *
+     * @param aid Activity主键
+     */
+    public void deleteAid(int aid) {
+        Session session = factory.getCurrentSession();
+        String sql = "DELETE FROM grade WHERE aid=" + aid;
+        session.createSQLQuery(sql);
+    }
+
+    /**
+     * 保存更新
+     *
+     * @param attendUsers 人员id
+     * @param activity    活动信息实例化
+     */
+    public void saveUpdate(List<String> attendUsers, Activity activity) {
+        Session session = factory.getCurrentSession();
+        for (String attendUser : attendUsers) {
+            Grade grade = new Grade();
+            grade.setUser(userDao.searchUser(Integer.parseInt(attendUser)));
+            grade.setActivity(activity);
+            session.save(grade);
+        }
+    }
 }
