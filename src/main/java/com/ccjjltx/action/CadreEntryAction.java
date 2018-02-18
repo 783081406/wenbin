bf1d730a98d8a4c05f77192568d0fc00cc117fd8
783081406 <783081406ccj@gmail.com>
2018-02-18 12:23:05 +0800

    增加update相关方法

--- src/main/java/com/ccjjltx/action/CadreEntryAction.java
+++ src/main/java/com/ccjjltx/action/CadreEntryAction.java
@@ -23,6 +23,11 @@ public class CadreEntryAction extends ActionSupport {
     @Resource(name = "userDao")
     private UserDao userDao;
     private List<User> result;//存放所有数据
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String type;
 
     public List<User> getResult() {
         return result;
@@ -30,6 +35,46 @@ public class CadreEntryAction extends ActionSupport {
 
     public void setResult(List<User> result) {
         this.result = result;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     /**
@@ -40,9 +85,42 @@ public class CadreEntryAction extends ActionSupport {
     public String getAllUser() {
         //得到所有数据
         setResult(userDao.getAllUser());
-        System.out.println(getResult());
         return SUCCESS;
     }
 
+    /**
+     * 更新User
+     *
+     * @return SUCCESS或Error
+     */
+    public String updateUser() {
+        return SUCCESS;
+    }
+
+    /**
+     * 保存更新
+     *
+     * @return SUCCESS
+     */
+    public String saveUpdate() {
+        try {
+            int type_temp = Integer.parseInt(getType());//类型只能是1或者2
+            if (type_temp == 1 || type_temp == 2) {
+                User db_user = userDao.searchUser(getId());//取User实例化数据
+                //更新数据
+                db_user.setName(getName());
+                db_user.setUsername(getUsername());
+                db_user.setPassword(getPassword());
+                db_user.setType(type_temp);
+                //执行更新
+                userDao.update(db_user);
+            } else {
+                return ERROR;
+            }
+        } catch (NumberFormatException e) {
+            return ERROR;
+        }
+        return SUCCESS;
+    }
 
 }
