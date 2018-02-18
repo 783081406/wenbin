cdbe35859b51d38c4a8a268d23932d02a800d007
783081406 <783081406ccj@gmail.com>
2018-02-18 22:26:28 +0800

    新增更新相关

--- src/main/java/com/ccjjltx/action/ActivityAction.java
+++ src/main/java/com/ccjjltx/action/ActivityAction.java
@@ -23,7 +23,12 @@ public class ActivityAction extends ActionSupport {
     @Resource(name = "activityDao")
     private ActivityDao activityDao;
     private List<Activity> result;//存放所有数据
+    private int aid;
     private String name;//搜索框提交过来的名字
+    private String place;
+    private String stime;
+    private String etime;
+    private String pdfpath;
 
     public List<Activity> getResult() {
         return result;
@@ -33,12 +38,52 @@ public class ActivityAction extends ActionSupport {
         this.result = result;
     }
 
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getStime() {
+        return stime;
+    }
+
+    public void setStime(String stime) {
+        this.stime = stime;
+    }
+
+    public String getEtime() {
+        return etime;
+    }
+
+    public void setEtime(String etime) {
+        this.etime = etime;
+    }
+
+    public String getPdfpath() {
+        return pdfpath;
+    }
+
+    public void setPdfpath(String pdfpath) {
+        this.pdfpath = pdfpath;
     }
 
     /**
@@ -59,6 +104,32 @@ public class ActivityAction extends ActionSupport {
      */
     public String search() {
         setResult(activityDao.search(getName()));
+        return SUCCESS;
+    }
+
+    /**
+     * 更新页面
+     *
+     * @return Success
+     */
+    public String update() {
+        return SUCCESS;
+    }
+
+    /**
+     * 更新操作
+     *
+     * @return Success
+     */
+    public String saveUpdate() {
+        Activity db_activity = activityDao.search(getAid());
+        //修改数据
+        db_activity.setName(getName());
+        db_activity.setPlace(getPlace());
+        db_activity.setStime(getStime());
+        db_activity.setEtime(getEtime());
+        //执行更新操作
+        activityDao.update(db_activity);
         return SUCCESS;
     }
 }
