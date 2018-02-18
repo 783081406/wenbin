22c3b1e4876b88af9a9d0337c908910e7e474b1d
783081406 <783081406ccj@gmail.com>
2018-02-18 21:13:24 +0800

    测试删除与新增方法

--- src/main/test/com/ccjjltx/dao/GradeDaoTest.java
+++ src/main/test/com/ccjjltx/dao/GradeDaoTest.java
@@ -1,5 +1,7 @@
 package com.ccjjltx.dao;
 
+import com.ccjjltx.domain.Activity;
+import com.ccjjltx.domain.Grade;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +10,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ccjjltx on 2018/2/18.
@@ -21,6 +25,8 @@ import javax.annotation.Resource;
 public class GradeDaoTest {
     @Resource(name = "gradeDao")
     private GradeDao gradeDao;
+    @Resource(name = "activityDao")
+    private ActivityDao activityDao;
 
     @Test
     @Transactional
@@ -28,5 +34,20 @@ public class GradeDaoTest {
         //得到项目id为1的参与人员，测试的时候的数据参与人员为1,2
         String result = gradeDao.attendUser(1);
         Assert.assertEquals("1,2", result);
+    }
+
+    @Test
+    @Transactional
+    public void testdeleteAid() {
+        gradeDao.deleteAid(1);
+    }
+
+    @Test
+    @Transactional
+    public void testSaveUpdate() {
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        gradeDao.saveUpdate(list, 2);
     }
 }
